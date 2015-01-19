b79223ef7c909acdee0623c41f3f37f26d727cd9
Sascha Peilicke <saschpe@mailbox.org>
2015-01-19 10:44:23 +0100

    Allow multiple verbosity levels.
    -v displays command execution info and -vv doesn' pass /silent to tf.

--- tft.py
+++ tft.py
@@ -40,7 +40,7 @@ INFO_LAST_MODIFIED_RE = re.compile('Last modified: (.*)\r')
 def destroy(args):
     tf_dir_cmd = TF_DIR_DELETED_FOLDERS_TEMPLATE.format(args.itemspec)
     output = subprocess.check_output(tf_dir_cmd, shell=True).decode()
-    if args.verbose:
+    if args.verbose and args.verbose > 0:
         print('Executing "{0}"...'.format(tf_dir_cmd))
     lines = output.split('\r\n')
     # Containing folder is always the first output line of 'tf dir'
@@ -49,7 +49,7 @@ def destroy(args):
         if ';X' in line:
             item = line.split(';X')[0][1:]  # Items look like '$123;X456', we care for 123
             absolute_item_path = '{0}/{1}'.format(absolute_folder_path, item)
-            if args.verbose:
+            if args.verbose and args.verbose > 0:
                 print('Found {0}'.format(absolute_item_path))
             tf_info_cmd = TF_INFO_TEMPLATE.format(absolute_item_path)
             output = subprocess.check_output(tf_info_cmd, shell=True).decode()
@@ -60,14 +60,16 @@ def destroy(args):
                     tf_destroy_cmd = TF_DESTROY_TEMPLATE.format(absolute_item_path)
                     if args.no_prompt:
                         tf_destroy_cmd += ' /noprompt'
-                    if args.verbose:
+                    if args.verbose and args.verbose < 2:
+                        tf_destroy_cmd += ' /silent'
+                    if args.verbose and args.verbose > 0:
                         print('Executing "{0}"...'.format(tf_destroy_cmd))
                     subprocess.call(tf_destroy_cmd, stdin=sys.stdin, stdout=sys.stdout, shell=True)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='TFS tool')
-    parser.add_argument('-v', '--verbose', action='store_true', help='enable verbose output')
+    parser.add_argument('-v', '--verbose', action='count', help='enable verbose output [-v or -vv]')
     parser.add_argument('-n', '--no-prompt', action='store_true', help='do not ask for confirmation')
 
     subparsers = parser.add_subparsers(help='sub-command help')
